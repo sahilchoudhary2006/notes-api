77f6ebedc1ed05dc282798433eaaf62cdbd5a188
sahilchoudhary2006 <sahil830261@gmail.com>
2026-08-16 15:35:10 +0530

    Implement note read APIs

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -1,16 +1,41 @@
-const getAllNotes = (req, res) => {
-    res.send("All Notes");
+import Note from "../models/note.models.js";
+
+const getAllNotes = async (req, res) => {
+    const notes = await Note.find();
+
+    res.status(200).json({
+    message: "Notes fetched successfully",
+    data: notes
+});
+
 };
 
-const createNote = (req, res) => {
+const getSingleNote = async (req, res) => {
+   
+    const note = await Note.findById(req.params.id);
 
-    console.log(req.body);
+    if(!note) {
+        return res.status(404).json({
+            message: "Note not found"
+        });
+    }
+
+    res.status(200).json({
+    message: "Note fetched successfully",
+    data: note
+});
+
+};
+
+const createNote = async (req, res) => {
+
+    const note = await Note.create(req.body);
 
     res.status(201).json({
         message: "Note created successfully",
-        data: req.body,
+        data: note,
     });
 };
 
-export { getAllNotes, createNote };
+export { getAllNotes, getSingleNote, createNote };
 
--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -2,12 +2,14 @@ import { Router } from "express";
 
 import {
     getAllNotes,
+    getSingleNote,
     createNote
 } from "../controllers/note.controller.js";
 
 const router = Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getSingleNote);
 
 router.post("/", createNote);
 
